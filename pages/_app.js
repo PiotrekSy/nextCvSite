2f61bba3e623fef82850c349e2337f6bf2eb2c3e
piotrekSy <piotrsyjud@gmail.com>
2023-03-29 04:54:26 +0200

    asd

--- pages/_app.js
+++ pages/_app.js
@@ -9,14 +9,18 @@ import '@fontsource/roboto/700.css';
 export default function App({ Component, pageProps }) {
   console.log('whole app render')
 
-  useEffect(() => {
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    const prefersLight = window.matchMedia('(prefers-color-scheme: light)').matches;
 
-    if (isMobile && !prefersLight) {
-      document.documentElement.setAttribute('data-theme', 'light');
-    }
-  }, []);
+  const prefersDark = window.matchMedia('(prefers-color-scheme: light)');
+
+  toggleDarkTheme(prefersDark.matches);
+
+  // Listen for changes to the prefers-color-scheme media query
+  prefersDark.addListener((mediaQuery) => toggleDarkTheme(mediaQuery.matches));
+
+  // Add or remove the "dark" class based on if the media query matches
+  function toggleDarkTheme(shouldAdd) {
+    document.body.classList.toggle('light', shouldAdd);
+  }
 
 
 
